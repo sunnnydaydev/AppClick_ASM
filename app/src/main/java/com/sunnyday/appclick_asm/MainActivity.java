9d08b030f55b4ab224b63e8300841f44043b1398
Administrator <1246771571@qq.com>
2020-07-08 20:26:02 +0800

    Initial commit

---
+++ app/src/main/java/com/sunnyday/appclick_asm/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sunnyday.appclick_asm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
