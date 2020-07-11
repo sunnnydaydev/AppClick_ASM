46f70f51f6629053d9ac966afd93e082f5295285
Administrator <1246771571@qq.com>
2020-07-11 20:26:03 +0800

    code update

--- mytransform/src/main/groovy/com/sunnyday/mytransform/MyTransform.groovy
+++ mytransform/src/main/groovy/com/sunnyday/mytransform/MyTransform.groovy
@@ -6,22 +6,40 @@ import com.android.build.api.transform.Transform
 import com.android.build.api.transform.TransformException
 import com.android.build.api.transform.TransformInput
 import com.android.build.api.transform.TransformOutputProvider
+import com.android.build.gradle.internal.pipeline.TransformManager
+import org.gradle.api.Project
+import org.gradle.api.artifacts.transform.TransformOutputs
 
-class MyTransform extends Transform{
+class MyTransform extends Transform {
+    private static  final String NAME = MyTransform.class.simpleName
+    private Project mProject
+
+    public MyTransform(Project project) {
+        this.mProject = project
+    }
 
     @Override
     String getName() {
-        return null
+        return NAME
     }
 
+    /**
+     * 需要处理的数据类型,有两种值：
+     * 1、TransformManager.CONTENT_CLASS
+     * 2、TransformManager.CONTENT_RESOURCES
+     *
+     * */
     @Override
     Set<QualifiedContent.ContentType> getInputTypes() {
-        return null
+        return TransformManager.CONTENT_CLASS
     }
 
+    /**
+     * Transform 要操作的内容范围，官方文档介绍有7种枚举类型。
+     * */
     @Override
     Set<? super QualifiedContent.Scope> getScopes() {
-        return null
+        return TransformManager.SCOPE_FULL_PROJECT
     }
 
     @Override
@@ -29,12 +47,33 @@ class MyTransform extends Transform{
         return false
     }
 
+    /**
+     *核心回调方法，主要在这里进行遍历获得class字节码文件，然后就可根据自己的意愿进行任何操作了。
+     *
+     * */
     @Override
     void transform(Context context,
                    Collection<TransformInput> inputs,
                    Collection<TransformInput> referencedInputs,
                    TransformOutputProvider outputProvider,
                    boolean isIncremental) throws IOException, TransformException, InterruptedException {
-        super.transform(context, inputs, referencedInputs, outputProvider, isIncremental)
+
+        printExtraInfo()
+        // transform 的input 有两种类型：目录与jar包。这二者要分开遍历。
+        inputs.each {
+            TransformInput transformInput->
+
+        }
+
+    }
+
+   static printExtraInfo(){
+        println()
+        println("######################################")
+        println("#######                        #######")
+        println("#######   Transform practise   #######")
+        println("#######                        #######")
+        println("######################################")
+
     }
 }
