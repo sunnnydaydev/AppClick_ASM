5e6687154bc57d47e3f95b71b5997bfd271b459d
Administrator <1246771571@qq.com>
2020-07-11 15:56:28 +0800

    transform practise

---
+++ mytransform/src/main/groovy/com/sunnyday/mytransform/MyPlugin.groovy
@@ -0,0 +1,11 @@
+package com.sunnyday.mytransform
+
+import org.gradle.api.Plugin
+
+class MyPlugin implements Plugin{
+
+    @Override
+    void apply(Object o) {
+
+    }
+}
---
+++ mytransform/src/main/groovy/com/sunnyday/mytransform/MyTransform.groovy
@@ -0,0 +1,40 @@
+package com.sunnyday.mytransform
+
+import com.android.build.api.transform.Context
+import com.android.build.api.transform.QualifiedContent
+import com.android.build.api.transform.Transform
+import com.android.build.api.transform.TransformException
+import com.android.build.api.transform.TransformInput
+import com.android.build.api.transform.TransformOutputProvider
+
+class MyTransform extends Transform{
+
+    @Override
+    String getName() {
+        return null
+    }
+
+    @Override
+    Set<QualifiedContent.ContentType> getInputTypes() {
+        return null
+    }
+
+    @Override
+    Set<? super QualifiedContent.Scope> getScopes() {
+        return null
+    }
+
+    @Override
+    boolean isIncremental() {
+        return false
+    }
+
+    @Override
+    void transform(Context context,
+                   Collection<TransformInput> inputs,
+                   Collection<TransformInput> referencedInputs,
+                   TransformOutputProvider outputProvider,
+                   boolean isIncremental) throws IOException, TransformException, InterruptedException {
+        super.transform(context, inputs, referencedInputs, outputProvider, isIncremental)
+    }
+}
