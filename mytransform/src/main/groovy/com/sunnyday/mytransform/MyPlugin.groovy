31a42f6f938ce68506a98e60d4b08921cd52bb3d
Administrator <1246771571@qq.com>
2020-07-12 20:09:58 +0800

    Transform 练习 finish

--- mytransform/src/main/groovy/com/sunnyday/mytransform/MyPlugin.groovy
+++ mytransform/src/main/groovy/com/sunnyday/mytransform/MyPlugin.groovy
@@ -1,11 +1,16 @@
 package com.sunnyday.mytransform
 
+import com.android.build.gradle.AppExtension
 import org.gradle.api.Plugin
+import org.gradle.api.Project
 
-class MyPlugin implements Plugin{
+class MyPlugin implements Plugin<Project>{
+
 
     @Override
-    void apply(Object o) {
-
+    void apply(Project project) {
+        // 注册自定义Transform
+       AppExtension appExtension = project.extensions.findByName(AppExtension.class)
+        appExtension.registerTransform(new MyTransform(project))
     }
 }
--- mytransform/src/main/groovy/com/sunnyday/mytransform/MyTransform.groovy
+++ mytransform/src/main/groovy/com/sunnyday/mytransform/MyTransform.groovy
@@ -1,6 +1,9 @@
 package com.sunnyday.mytransform
 
 import com.android.build.api.transform.Context
+import com.android.build.api.transform.DirectoryInput
+import com.android.build.api.transform.Format
+import com.android.build.api.transform.JarInput
 import com.android.build.api.transform.QualifiedContent
 import com.android.build.api.transform.Transform
 import com.android.build.api.transform.TransformException
@@ -9,9 +12,21 @@ import com.android.build.api.transform.TransformOutputProvider
 import com.android.build.gradle.internal.pipeline.TransformManager
 import org.gradle.api.Project
 import org.gradle.api.artifacts.transform.TransformOutputs
+import org.gradle.internal.impldep.org.apache.commons.codec.digest.DigestUtils
+import org.gradle.internal.impldep.org.apache.commons.io.FileUtils
+/**
+ * 简单的目录拷贝实践
+ *
+ * 注意：
+ * 即使我们什么都没有做，
+ * 也需要把所有的输入文件拷贝到目标目录下，
+ * 否则下一个Task就没有TransformInput了。
+ * 在这个实例中，如果我们空实现了transform方法，
+ * 最后会导致打包的apk缺少.class文件。
+ * */
 
 class MyTransform extends Transform {
-    private static  final String NAME = MyTransform.class.simpleName
+    private static final String NAME = MyTransform.class.simpleName
     private Project mProject
 
     public MyTransform(Project project) {
@@ -20,13 +35,13 @@ class MyTransform extends Transform {
 
     @Override
     String getName() {
-        return NAME
+        return NAME // Transform 对应的task name
     }
 
     /**
      * 需要处理的数据类型,有两种值：
-     * 1、TransformManager.CONTENT_CLASS
-     * 2、TransformManager.CONTENT_RESOURCES
+     * 1、TransformManager.CONTENT_CLASS：java class文件
+     * 2、TransformManager.CONTENT_RESOURCES：资源文件
      *
      * */
     @Override
@@ -39,6 +54,8 @@ class MyTransform extends Transform {
      * */
     @Override
     Set<? super QualifiedContent.Scope> getScopes() {
+        //SCOPE_FULL_PROJECT = ImmutableSet.of(Scope.PROJECT, Scope.SUB_PROJECTS, Scope.EXTERNAL_LIBRARIES);
+        //这个常量值的范围包括三部分：当前项目、子项目以及外部的依赖库。
         return TransformManager.SCOPE_FULL_PROJECT
     }
 
@@ -48,7 +65,7 @@ class MyTransform extends Transform {
     }
 
     /**
-     *核心回调方法，主要在这里进行遍历获得class字节码文件，然后就可根据自己的意愿进行任何操作了。
+     * 核心回调方法，主要在这里进行遍历获得class字节码文件，然后就可根据自己的意愿进行任何操作了。
      *
      * */
     @Override
@@ -61,13 +78,44 @@ class MyTransform extends Transform {
         printExtraInfo()
         // transform 的input 有两种类型：目录与jar包。这二者要分开遍历。
         inputs.each {
-            TransformInput transformInput->
-
+            TransformInput transformInput ->
+                // 1、遍历目录
+                //遍历目录，获取outPut目录
+                transformInput.directoryInputs.each {
+                    DirectoryInput directoryInput ->
+                        // 生成输出路径
+                        def dest = outputProvider.getContentLocation(
+                                directoryInput.name,
+                                directoryInput.contentTypes,
+                                directoryInput.scopes,
+                                Format.DIRECTORY)
+                        // 将input目录复制到outPut目录
+                        FileUtils.copyDirectory(directoryInput.file, dest)
+                }
+                // 遍历jar包
+                transformInput.jarInputs.each {
+                    JarInput jarInput ->
+                        // 重命名输出文件,因为同目录下copy file 会冲突
+                        def jarName = jarInput.name
+                        def md5Name = DigestUtils.md5Hex(jarInput.file.absolutePath)
+                        if (jarName.endsWith(".jar")) {
+                            jarName = jarName.substring(0, jarName.length() - 4)
+                        }
+                        //生成输出路径
+                        def dest = outputProvider.getContentLocation(
+                                jarName + md5Name,
+                                JarInput.ContentType,
+                                JarInput.Scope,
+                                Format.JAR
+                        )
+                        FileUtils.copyDirectory(jarInput.file, dest)
+                }
         }
+
 
     }
 
-   static printExtraInfo(){
+    static printExtraInfo() {
         println()
         println("######################################")
         println("#######                        #######")
